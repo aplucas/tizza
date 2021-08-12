9320935637aca5e8aa7ca65cc56ea05dd0df0885
Lucas A Pereira <l.alexandre100@gmail.com>
2021-08-12 07:56:48 -0300

    Listagem 2.9 login e logout

--- tizza/urls.py
+++ tizza/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.contrib.auth.views import LoginView, LogoutView
 from user.views import SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pizzas/', include('pizza.urls')),
     path('register/', SignupView.as_view()),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
-
